9f5a20fae77cfac52515bf9630144badf9bf0123
JasonKo <jason@kosbrother.com>
2023-06-26 16:07:05 +0800

    fix

--- app/web/urls.py
+++ app/web/urls.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('process_price', views.process_price, name='process_price'),
+    path('types', views.types, name='types'),
+    path('featured_case', views.featured_case, name='featured_case'),
+    path('q_and_a', views.q_and_a, name='q_and_a'),
+    path('contact', views.contact, name='contact'),
 ]
--- app/web/views.py
+++ app/web/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'web/index.html')
+
+def about(request):
+    return render(request,'web/about.html')
+
+def process_price(request):
+    data = "傳到html的資料"
+    return render(request,'web/process_price.html',{'data':data})
+
+def types(request):
+    style = request.GET.get('style')
+    return render(request,'web/types.html',{'style':style})
+
+def featured_case(request):
+    return render(request,'web/featured_case.html')
+
+def q_and_a(request):
+    return render(request,'web/q_and_a.html')
+
+def contact(request):
+    return render(request,'web/contact.html')
